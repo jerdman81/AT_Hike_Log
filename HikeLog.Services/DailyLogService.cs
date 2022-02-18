cc2f3f876eb65badb1499ca481626a6aa8b4c068
Jeff Erdman <jerdman81@gmail.com>
2022-02-18 09:41:58 -0500

    DailyLog ListItem and Create completed.

---
+++ HikeLog.Services/DailyLogService.cs
@@ -0,0 +1,46 @@
+﻿using HikeLog.Data;
+using HikeLog.Models.DailyLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HikeLog.Services
+{
+    public class DailyLogService
+    {
+        private readonly Guid _userid;
+
+        public DailyLogService(Guid userId)
+        {
+            _userid = userId;
+        }
+
+        public IEnumerable<DailyLogListItem> GetDailyLogs()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .DailyLogs
+                        .Select(
+                            e =>
+                                new DailyLogListItem
+                                {
+                                    DailyLogId = e.DailyLogId,
+                                    ProfileId = e.ProfileId,
+                                    SectionId = e.SectionId,
+                                    Date = e.Date,
+                                    StartMile = e.StartMile,
+                                    EndMile = e.EndMile,
+                                    Notes = e.Notes,
+                                    IsStarred = e.IsStarred,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
---
+++ HikeLog.WebMVC/Controllers/DailyLogController.cs
@@ -0,0 +1,30 @@
+﻿using HikeLog.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HikeLog.WebMVC.Controllers
+{
+    public class DailyLogController : Controller
+    {
+        [Authorize]
+
+        // GET: DailyLog
+        public ActionResult Index()
+        {
+            var service = CreateDailyLogService();
+            var model = service.GetDailyLogs();
+            return View(model);
+        }
+
+        private DailyLogService CreateDailyLogService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new DailyLogService(userId);
+            return service;
+        }
+    }
+}
